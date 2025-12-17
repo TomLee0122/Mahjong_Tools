3229d9d2c7fedccabf25b08edee95ddedb5bb1d6
Yikun Li <liyikun0122@gmail.com>
2025-12-17 17:15:59 -0600

    Improvement on the algorithm structure

--- class_definitions.py
+++ class_definitions.py
@@ -7,31 +7,26 @@ import utilities
 
 class Mahjong_Cards():
     # Read the cards in
-    def __init__(self):
-        self.p_cards = utilities.initialize_card_dict('p')
-        self.p_card_num = 0
-        self.m_cards = utilities.initialize_card_dict('m')
-        self.m_card_num = 0
-        self.s_cards = utilities.initialize_card_dict('s')
-        self.s_card_num = 0
-        self.z_cards = utilities.initialize_card_dict('z')
-        self.z_card_num = 0
-        self.raw_card_string = None
-        self.card_list = None
-        self.card_count = None
+    def __init__(self, separator: str = ' ') -> None:
+        self.separator = separator
+        self.card_info = {'p': utilities.initialize_card_dict('p'), 'm': utilities.initialize_card_dict(
+            'm'), 's': utilities.initialize_card_dict('s'), 'z': utilities.initialize_card_dict('z')}
+        self.num_info = {'p': 0, 'm': 0, 's': 0, 'z': 0}
+        return
 
-    def check_correctness_cards(self):
+    def check_correctness_cards(self) -> None:
         if self.card_count != CURRENT_CARD_NUM:
             raise errors.Mahjong_Card_Num_Error(self.card_count)
-        for current_check_type in [self.p_cards, self.m_cards, self.s_cards, self.z_cards]:
+        for current_check_type in self.card_info.values():
             temp_length = len(current_check_type)
             for i in range(temp_length):
                 if current_check_type[i] > SINGLE_CARD_MAX_NUM:
                     raise errors.Single_Card_Num_Error(current_check_type[i])
+        return
 
-    def read_cards(self, raw_card_string: str, check: bool = True):
+    def read_cards(self, raw_card_string: str, check: bool = True) -> None:
         self.raw_card_string = raw_card_string
-        self.card_list = raw_card_string.split(' ')
+        self.card_list = raw_card_string.split(self.separator)
         self.card_count = len(self.card_list)
 
         for current_cards in self.card_list:
@@ -44,104 +39,87 @@ class Mahjong_Cards():
                 else:
                     continue
 
-            if current_card_type == 'p':
-                if check and (current_card_number > 9 or current_card_number < 0):
-                    raise errors.Undefined_Mahjong_Card_Error(
-                        str(current_card_number)+current_card_type)
-                self.p_cards[current_card_number] += 1
-                self.p_card_num += 1
-                continue
-            if current_card_type == 'm':
-                if check and (current_card_number > 9 or current_card_number < 0):
-                    raise errors.Undefined_Mahjong_Card_Error(
-                        str(current_card_number)+current_card_type)
-                self.m_cards[current_card_number] += 1
-                self.m_card_num += 1
-                continue
-            if current_card_type == 's':
-                if check and (current_card_number > 9 or current_card_number < 0):
-                    raise errors.Undefined_Mahjong_Card_Error(
-                        str(current_card_number)+current_card_type)
-                self.s_cards[current_card_number] += 1
-                self.s_card_num += 1
-                continue
-            if current_card_type == 'z':
-                if check and (current_card_number > 9 or current_card_number < 0):
-                    raise errors.Undefined_Mahjong_Card_Error(
-                        str(current_card_number)+current_card_type)
-                self.z_cards[current_card_number] += 1
-                self.z_card_num += 1
-                continue
-            if check:
-                raise errors.Undefined_Mahjong_Card_Error(current_card_type)
+            if current_card_number > 9 or current_card_number < 0 or current_card_type not in ['p', 'm', 's', 'z']:
+                if check:
+                    raise errors.Undefined_Mahjong_Card_Error(current_cards)
+            else:
+                self.card_info[current_card_type][current_card_number] += 1
+                self.num_info[current_card_type] += 1
 
         if check:
             self.check_correctness_cards()
 
-        self.p_cards[5] += self.p_cards[0]
-        self.m_cards[5] += self.m_cards[0]
-        self.s_cards[5] += self.s_cards[0]
+        self.card_info['p'][5] += self.card_info['p'][0]
+        self.card_info['m'][5] += self.card_info['m'][0]
+        self.card_info['s'][5] += self.card_info['s'][0]
 
-        self.card_info = {'p': self.p_cards, 'm': self.m_cards,
-                          's': self.s_cards, 'z': self.z_cards}
+        return
 
-    def check_which_tenpai_on(self, card_type: str, check_type: str = 'com_w_pair'):
+    def check_which_tenpai_on(self, card_type: str, check_type: str) -> list[str]:
         assert card_type in ['p', 'm', 's', 'z']
         assert check_type in ['com', 'com_w_pair']
         tenpai_on_list = []
         for current_card in self.card_info[card_type]:
-            temp_card_dict = copy.copy(self.card_info[card_type])
-            temp_card_dict[current_card] += 1
+            self.card_info[card_type][current_card] += 1
             if check_type == 'com':
-                if utilities.check_if_complete_set(temp_card_dict, card_type, None)[check_type]:
+                if utilities.check_if_complete_set(self.card_info[card_type], card_type)[check_type]:
                     tenpai_on_list.append(str(current_card) + card_type)
             else:
-                if utilities.check_if_complete_set(temp_card_dict, card_type, None)[check_type][0]:
+                if utilities.check_if_complete_set(self.card_info[card_type], card_type)[check_type][0]:
                     tenpai_on_list.append(str(current_card) + card_type)
+            self.card_info[card_type][current_card] -= 1
         return tenpai_on_list
 
-    def check_if_tenpai(self):
-        self.p_status = utilities.check_if_complete_set(self.p_cards, 'p')
-        self.m_status = utilities.check_if_complete_set(self.m_cards, 'm')
-        self.s_status = utilities.check_if_complete_set(self.s_cards, 's')
-        self.z_status = utilities.check_if_complete_set(self.z_cards, 'z')
-        self.status_dict = {
-            'p': self.p_status, 'm': self.m_status, 's': self.s_status, 'z': self.z_status}
-        self.tenpai_status = {'incom': [], 'com': [], 'com_w_pair': []}
+    def check_if_tenpai(self) -> list:
+        p_status = utilities.check_if_complete_set(
+            self.card_info['p'], 'p')
+        m_status = utilities.check_if_complete_set(
+            self.card_info['m'], 'm')
+        s_status = utilities.check_if_complete_set(
+            self.card_info['s'], 's')
+        z_status = utilities.check_if_complete_set(
+            self.card_info['z'], 'z')
 
-        for current_status in list(self.status_dict.items()):
-            if current_status[1]['incom']:
-                self.tenpai_status['incom'].append(current_status[0])
-            if current_status[1]['com']:
-                self.tenpai_status['com'].append(current_status[0])
-            if current_status[1]['com_w_pair'][0]:
-                self.tenpai_status['com_w_pair'].append(current_status[0])
+        status_dict = {
+            'p': p_status, 'm': m_status, 's': s_status, 'z': z_status}
+        tenpai_status = {'incom': [], 'com': [], 'com_w_pair': []}
 
-        self.incom_count = len(self.tenpai_status['incom'])
-        self.com_count = len(self.tenpai_status['com'])
-        self.com_w_pair_count = len(self.tenpai_status['com_w_pair'])
+        for card_type, status in status_dict.items():
+            if status['incom']:
+                tenpai_status['incom'].append(card_type)
+            if status['com']:
+                tenpai_status['com'].append(card_type)
+            if status['com_w_pair'][0]:
+                tenpai_status['com_w_pair'].append(card_type)
+        incom_count = len(tenpai_status['incom'])
+        # com_count = len(tenpai_status['com'])
+        com_w_pair_count = len(tenpai_status['com_w_pair'])
 
-        if self.incom_count == 0 and self.com_w_pair_count == 2:
+        if incom_count == 0 and com_w_pair_count == 2:
             tenpai_list_1 = self.check_which_tenpai_on(
-                self.tenpai_status['com_w_pair'][0], check_type='com')
+                tenpai_status['com_w_pair'][0], check_type='com')
             tenpai_list_2 = self.check_which_tenpai_on(
-                self.tenpai_status['com_w_pair'][1], check_type='com')
+                tenpai_status['com_w_pair'][1], check_type='com')
             return [True, tenpai_list_1+tenpai_list_2]
-        if self.incom_count == 1 and self.com_w_pair_count == 0:
+
+        if incom_count == 1 and com_w_pair_count == 0:
             tenpai_list_1 = self.check_which_tenpai_on(
-                self.tenpai_status['incom'][0], check_type='com_w_pair')
-            if len(tenpai_list_1) >= 1:
+                tenpai_status['incom'][0], check_type='com_w_pair')
+            if tenpai_list_1:
                 return [True, tenpai_list_1]
-        if self.incom_count == 1 and self.com_w_pair_count == 1:
+
+        if incom_count == 1 and com_w_pair_count == 1:
             tenpai_list_1 = self.check_which_tenpai_on(
-                self.tenpai_status['incom'][0], check_type='com')
-            if len(tenpai_list_1) >= 1:
+                tenpai_status['incom'][0], check_type='com')
+            if tenpai_list_1:
                 return [True, tenpai_list_1]
+
         return [False]
 
 
 if __name__ == '__main__':
-    example_1 = '1m 1m 1m 2m 3m 4m 5m 6m 7m 7m 9m 9m 9m'
+    # example_1 = "1m 1m 2m 2m 3s 4s 5s 6p 7p 8p 9p 9p 9p"
+    example_1 = "1m 1m 1m 2m 3m 4m 5m 6m 7m 8m 9m 9m 9m"
     my_cards = Mahjong_Cards()
     my_cards.read_cards(example_1)
     print(my_cards.check_which_tenpai_on('m', check_type='com_w_pair'))
--- utilities.py
+++ utilities.py
@@ -1,4 +1,3 @@
-import os
 import errors
 import copy
 
@@ -26,165 +25,141 @@ def incomplete_operator(complete_status: dict):
     complete_status['com_w_pair'] = [False, []]
     return complete_status
 
-
-def turn_incomplete(complete_status: dict):
-    assert complete_status.keys() == {'incom', 'com', 'com_w_pair'}
-    complete_status['incom'] = True
-    return complete_status
+# Use depth-first search to check if the card set is complete
 
 
-def check_if_complete_set(card_dict: dict, card_type: str, complete_status: dict = None):
-    if complete_status is None:
-        complete_status = initialize_complete_status()
+def check_if_complete_set(card_dict: dict, card_type: str) -> dict:
+    complete_status = initialize_complete_status()
+    if card_type not in ['p', 'm', 's', 'z']:
+        raise errors.Undefined_Mahjong_Card_Error(card_type)
     card_num = 0
     for i in card_dict:
         card_num += card_dict[i]
-    if card_type not in ['p', 'm', 's', 'z']:
-        raise errors.Undefined_Mahjong_Card_Error(card_type)
     if card_num == 0:
         if not complete_status['com_w_pair'][0]:
             complete_status['com'] = True
         return complete_status
     if card_num % 3 == 1:
-        complete_status = turn_incomplete(complete_status)
+        complete_status = incomplete_operator(complete_status)
         return complete_status
+    # Find the first non-zero number card
+    i = 1
+    while card_dict[i] == 0:
+        i += 1
+
     if card_type == 'z':
-        for i in range(1, 8):
-            if card_dict[i] != 0:
-                if card_dict[i] == 1:
-                    complete_status = incomplete_operator(complete_status)
-                    return complete_status
-                if card_dict[i] == 2:
-                    if complete_status['com_w_pair'][0]:
-                        complete_status = incomplete_operator(complete_status)
-                        return complete_status
-                    complete_status['com_w_pair'] = [True, [i]]
-                    temp_dict = card_dict.copy()
-                    temp_dict[i] -= 2
-                    complete_status = check_if_complete_set(
-                        temp_dict, card_type, complete_status)
-                    if complete_status['incom']:
-                        complete_status = incomplete_operator(complete_status)
-                    return complete_status
-                if card_dict[i] == 3:
-                    temp_dict = card_dict.copy()
-                    temp_dict[i] -= 3
-                    complete_status = check_if_complete_set(
-                        temp_dict, card_type, complete_status)
-                    return complete_status
+        if card_dict[i] == 1:
+            complete_status = incomplete_operator(complete_status)
+            return complete_status
+        elif card_dict[i] == 2:
+            card_dict[i] -= 2
+            temp_complete_status = check_if_complete_set(
+                card_dict, card_type)
+            card_dict[i] += 2
+            if temp_complete_status['com']:
+                complete_status['com_w_pair'][0] = True
+                complete_status['com_w_pair'][1].append(i)
+            else:
+                complete_status = incomplete_operator(complete_status)
+            return complete_status
+        else:
+            card_dict[i] -= 3
+            temp_complete_status = check_if_complete_set(
+                card_dict, card_type)
+            card_dict[i] += 3
+            if temp_complete_status['com']:
+                complete_status['com'] = True
+            elif temp_complete_status['com_w_pair'][0]:
+                complete_status['com_w_pair'][0] = True
+                complete_status['com_w_pair'][1] += temp_complete_status['com_w_pair'][1]
+            else:
+                complete_status = incomplete_operator(complete_status)
+            return complete_status
     else:
-        for i in range(1, 10):
-            if card_dict[i] != 0:
-                if card_num % 3 == 0:
-                    if card_dict[i] >= 3:
-                        temp_dict = card_dict.copy()
-                        temp_dict[i] -= 3
-                        complete_status = check_if_complete_set(
-                            temp_dict, card_type, complete_status)
-                        if complete_status['com']:
-                            return complete_status
-                    temp_dict = card_dict.copy()
-                    if i <= 7 and card_dict[i+1] >= 1 and card_dict[i+2] >= 1:
-                        temp_dict[i] -= 1
-                        temp_dict[i+1] -= 1
-                        temp_dict[i+2] -= 1
-                        complete_status = check_if_complete_set(
-                            temp_dict, card_type, complete_status)
-                        if complete_status['com']:
-                            return complete_status
-                    else:
-                        complete_status = turn_incomplete(complete_status)
-                        return complete_status
-                elif card_num % 3 == 2:
-                    if card_dict[i] >= 3:
-                        temp_dict = card_dict.copy()
-                        temp_dict[i] -= 3
-                        temp_complete_status = initialize_complete_status()
-                        temp_complete_status = check_if_complete_set(
-                            temp_dict, card_type, temp_complete_status)
-                        if temp_complete_status['com_w_pair'][0]:
-                            complete_status['com_w_pair'][0] = True
-                            complete_status['com_w_pair'][1] += temp_complete_status['com_w_pair'][1]
-                        temp_dict = card_dict.copy()
-                        temp_dict[i] -= 2
-                        temp_complete_status = initialize_complete_status()
-                        temp_complete_status = check_if_complete_set(
-                            temp_dict, card_type, temp_complete_status)
-                        if temp_complete_status['com']:
-                            complete_status['com_w_pair'][0] = True
-                            complete_status['com_w_pair'][1].append(i)
-                        if i <= 7 and card_dict[i+1] >= 1 and card_dict[i+2] >= 1:
-                            temp_dict = card_dict.copy()
-                            temp_dict[i] -= 1
-                            temp_dict[i+1] -= 1
-                            temp_dict[i+2] -= 1
-                            temp_complete_status = initialize_complete_status()
-                            temp_complete_status = check_if_complete_set(
-                                temp_dict, card_type, temp_complete_status)
-                            if temp_complete_status['com_w_pair'][0]:
-                                complete_status['com_w_pair'][0] = True
-                                complete_status['com_w_pair'][1] += temp_complete_status['com_w_pair'][1]
-                    elif card_dict[i] == 2:
-                        temp_dict = card_dict.copy()
-                        temp_dict[i] -= 2
-                        temp_complete_status = initialize_complete_status()
-                        temp_complete_status = check_if_complete_set(
-                            temp_dict, card_type, temp_complete_status)
-                        if temp_complete_status['com']:
-                            complete_status['com_w_pair'][0] = True
-                            complete_status['com_w_pair'][1].append(i)
-                        if i <= 7 and card_dict[i+1] >= 1 and card_dict[i+2] >= 1:
-                            temp_dict = card_dict.copy()
-                            temp_dict[i] -= 1
-                            temp_dict[i+1] -= 1
-                            temp_dict[i+2] -= 1
-                            temp_complete_status = initialize_complete_status()
-                            temp_complete_status = check_if_complete_set(
-                                temp_dict, card_type, temp_complete_status)
-                            if temp_complete_status['com_w_pair'][0]:
-                                complete_status['com_w_pair'][0] = True
-                                complete_status['com_w_pair'][1] += temp_complete_status['com_w_pair'][1]
-                    elif card_dict[i] == 1:
-                        if i <= 7 and card_dict[i+1] >= 1 and card_dict[i+2] >= 1:
-                            temp_dict = card_dict.copy()
-                            temp_dict[i] -= 1
-                            temp_dict[i+1] -= 1
-                            temp_dict[i+2] -= 1
-                            temp_complete_status = initialize_complete_status()
-                            temp_complete_status = check_if_complete_set(
-                                temp_dict, card_type, temp_complete_status)
-                            if temp_complete_status['com_w_pair'][0]:
-                                complete_status['com_w_pair'][0] = True
-                                complete_status['com_w_pair'][1] += temp_complete_status['com_w_pair'][1]
-                    if complete_status['com_w_pair'][0]:
-                        complete_status['incom'] = False
-                        complete_status['com_w_pair'][1] = list(
-                            set(complete_status['com_w_pair'][1]))
-                    else:
-                        complete_status = incomplete_operator(complete_status)
+        if card_num % 3 == 0:
+            if card_dict[i] >= 3:
+                card_dict[i] -= 3
+                temp_complete_status = check_if_complete_set(
+                    card_dict, card_type)
+                card_dict[i] += 3
+                if temp_complete_status['com']:
+                    complete_status['com'] = True
                     return complete_status
+            if i <= 7 and card_dict[i+1] >= 1 and card_dict[i+2] >= 1:
+                card_dict[i] -= 1
+                card_dict[i+1] -= 1
+                card_dict[i+2] -= 1
+                temp_complete_status = check_if_complete_set(
+                    card_dict, card_type)
+                card_dict[i] += 1
+                card_dict[i+1] += 1
+                card_dict[i+2] += 1
+                if temp_complete_status['com']:
+                    complete_status['com'] = True
+                    return complete_status
+            complete_status = incomplete_operator(complete_status)
+            return complete_status
+        # card_num % 3 == 2
+        else:
+            if card_dict[i] >= 3:
+                card_dict[i] -= 3
+                temp_complete_status = check_if_complete_set(
+                    card_dict, card_type)
+                card_dict[i] += 3
+                if temp_complete_status['com_w_pair'][0]:
+                    complete_status['com_w_pair'][0] = True
+                    complete_status['com_w_pair'][1] += temp_complete_status['com_w_pair'][1]
+            if card_dict[i] >= 2:
+                card_dict[i] -= 2
+                temp_complete_status = check_if_complete_set(
+                    card_dict, card_type)
+                card_dict[i] += 2
+                if temp_complete_status['com']:
+                    complete_status['com_w_pair'][0] = True
+                    complete_status['com_w_pair'][1].append(i)
+            if card_dict[i] >= 1:
+                if i <= 7 and card_dict[i+1] >= 1 and card_dict[i+2] >= 1:
+                    card_dict[i] -= 1
+                    card_dict[i+1] -= 1
+                    card_dict[i+2] -= 1
+                    temp_complete_status = check_if_complete_set(
+                        card_dict, card_type)
+                    card_dict[i] += 1
+                    card_dict[i+1] += 1
+                    card_dict[i+2] += 1
+                    if temp_complete_status['com_w_pair'][0]:
+                        complete_status['com_w_pair'][0] = True
+                        complete_status['com_w_pair'][1] += temp_complete_status['com_w_pair'][1]
+            if complete_status['com_w_pair'][0]:
+                complete_status['incom'] = False
+                complete_status['com_w_pair'][1] = list(
+                    set(complete_status['com_w_pair'][1]))
+            else:
+                complete_status = incomplete_operator(complete_status)
+            return complete_status
 
 
 if __name__ == '__main__':
-    example_1 = {i: 0 for i in range(1, 8)}
-    example_1[2] = 2
-    example_1[3] = 0
-    example_1[4] = 0
-    example_1[6] = 0
-    print(check_if_complete_set(example_1, 'z'))
-    example_2 = {i: 0 for i in range(1, 10)}
+    # example_1 = {i: 0 for i in range(1, 8)}
+    # example_1[2] = 3
+    # example_1[3] = 3
+    # example_1[4] = 2
+    # example_1[6] = 3
+    # print(check_if_complete_set(example_1, 'z'))
+    example_2 = {i: 0 for i in range(10)}
+    example_2[1] = 3
+    example_2[2] = 1
     example_2[3] = 1
-    example_2[4] = 2
-    example_2[5] = 2
+    example_2[8] = 3
     print(check_if_complete_set(example_2, 's'))
-    example_3 = {i: 0 for i in range(1, 10)}
-    example_3[1] = 4
-    example_3[2] = 1
-    example_3[3] = 1
-    example_3[4] = 1
-    example_3[5] = 1
-    example_3[6] = 1
-    example_3[7] = 1
-    example_3[8] = 1
-    example_3[9] = 3
-    print(check_if_complete_set(example_3, 'm'))
+    # example_3 = {i: 0 for i in range(1, 10)}
+    # example_3[1] = 4
+    # example_3[2] = 1
+    # example_3[3] = 1
+    # example_3[4] = 1
+    # example_3[5] = 1
+    # example_3[6] = 1
+    # example_3[7] = 1
+    # example_3[8] = 1
+    # example_3[9] = 3
+    # print(check_if_complete_set(example_3, 'm'))
